566a62700ca7ee7bfd456a823591ad9e71ee275a
Davylander <davisouto.dev@gmail.com>
2022-05-14 17:42:48 -0300

    Adicionando mais coisas para a Conexão

--- src/diario/ConexaoDiario.java
+++ src/diario/ConexaoDiario.java
@@ -9,15 +9,32 @@ public class ConexaoDiario {
 	private Connection conexao;
 	
 	ConexaoDiario(){
-		url = "jdbc:postresql://localhost:5432/?";
+		url = "jdbc:postresql://localhost:5432/diario";
 		usuario = "postgres";
 		senha = "2908";
 		
 		try {
-			
+			Class.forName("org.postgresql.Driver");
+			conexao = DriverManager.getConnection(url,usuario,senha);
+			System.out.println("Conexão realizada com sucesso!");
 		}catch(Exception e) {
 			e.printStackTrace();		
 		}
+	}
+	
+	public int executarSQL(String sql) {
+		try {
+			Statement statement = conexao.createStatement();
+			int resposta = statement.executeUpdate(sql);
+			return resposta;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+	public ResultSet executaBuscaSQL(String sql) {
+		
 	}
 	
 }
